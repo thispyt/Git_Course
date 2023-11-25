fe32f2f6ad81494cbcc6b016f69c3ecb2443b1a1
thispyt <paassaas21@gmail.com>
2023-11-25 08:09:31 +0100

    thispyt

---
+++ pythonlearn/basic.py
@@ -0,0 +1,20 @@
+
+# # # # exemple.py
+
+# # # def hello(name):
+# # #     print("Hello, {name}!")
+
+# # # hello("World")
+# # print('heello'); print("where are")
+# # x=5 
+# print(type(2.0))
+# print(type([1,2,3,4,5]))
+# print(type((1,2,3,4,5)))
+# print({'amine\b':1,'two':2})
+# name='ami\bne amine'
+# print(name)
+myString="iphone"
+print(myString[0:4])
+print(myString[:])
+print(myString[:3])
+print(myString[2:])
